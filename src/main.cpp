f8f6dcdba6e048615ae4c848f24821453dc59139
Aisen Burnashev <oruzejnik@gmail.com>
2023-09-14 13:33:45 +0700

    Fix numbers parameters(size and so on) and added time elapsed benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,39 @@
 #include "pch.h"
 
+#include "clock/Clock.h"
 #include "Utils.h"
 
 #include "cuda/CudaUtils.h"
 
-const size_t ARRAY_SIZE = 100;
+const size_t ARRAY_SIZE = 10000000;
 const unsigned MIN_UNIQUE = 10;
-const unsigned MAX_UNIQUE = 50;
+const unsigned MAX_UNIQUE = 1000;
 
 int main() {
     // Init random source array.
+    Clock clock;
+
+//+_+_+_-=-=- =- Array generation  =- =- == = -=- =- =- =- 
+
+    auto arrayGenStart = clock.Now();
+
     auto srcArray = ai::utils::GenerateArray(ARRAY_SIZE, MIN_UNIQUE, MAX_UNIQUE);
+    
+    clock.PrintDurationFrom(arrayGenStart);
     ai::utils::PrintArray("Source array", srcArray, 10);
 
+
+// -=-= -=- =-= Looking for uniques -= -=- =-=- =- =-= -=- =- =- =- =- =-= -
+
+    auto searchUniquesStart = clock.Now();
+
     auto uniques = ai::cuda::FindUniquesGPU(srcArray);
+
+    clock.PrintDurationFrom(searchUniquesStart);
     ai::utils::PrintArray("Uniques", uniques, 2);
+
+
+// -=- =-= -=- =- =-  -= -- = -= -= -=- =- 
 
     std::cin.get();
     return 0;
